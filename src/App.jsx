8fa542bb0c4e1e96c8a5bb556127fb4eb26eaab7
Ritika-trivedi <ritikatrivedi33@gmail.com>
2024-09-25 15:02:41 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./Component/Home";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./Component/Login";
 import Account from "./Component/Account";
 import UserList from "./Component/UserList";
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/account" element={<Account></Account>}></Route>
-        <Route path="/userlist" element={<UserList />}></Route>
-      </Routes>
-    </div>
+      <HashRouter>
+        {" "}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/account" element={<Account></Account>}></Route>
+          <Route path="/userlist" element={<UserList />}></Route>
+        </Routes>
+      </HashRouter>
+      </div>
   );
 };
 
